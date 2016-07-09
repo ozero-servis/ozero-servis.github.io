a7c700537b4ac900c0e57775b1b439d9812ec5e4
ozero-servis <eduardyaremcuk@gmail.com>
2016-07-09 19:00:23 +0300

    New version updates 01

---
+++ usluga/js/main.js
@@ -0,0 +1,52 @@
+jQuery(document).ready(function($){
+	// browser window scroll (in pixels) after which the "back to top" link is shown
+	var offset = 1000, //300
+		//browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+
+		//offset_opacity = 1200, <-------
+
+		//duration of the top scrolling animation (in ms)
+		scroll_top_duration = 400, //700
+		//grab the "back to top" link
+		$back_to_top = $('.cd-top');
+
+	//hide or show the "back to top" link <----------
+	/*$(window).scroll(function(){
+		( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
+		if( $(this).scrollTop() > offset_opacity ) { 
+			$back_to_top.addClass('cd-fade-out');
+		}
+	});*/
+
+	//hide or show the "back to top" link
+	$(window).scroll(function(){
+		( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible');
+	});
+
+	//smooth scroll to top
+	$back_to_top.on('click', function(event){
+		event.preventDefault();
+		$('body,html').animate({
+			scrollTop: 0 ,
+		 	}, scroll_top_duration
+		);
+	});
+
+
+	//fixed nav
+	//var card = $('#card');
+	var card = $('#mrgn');
+	var secondaryNav = $('#nav'),
+		secondaryNavTopPosition = secondaryNav.offset().top;
+ 
+	$(window).on('scroll', function(){
+		if($(window).scrollTop() > secondaryNavTopPosition ) {
+			secondaryNav.addClass('is-fixed');
+			card.addClass('has-top-margin');
+		} else {
+			secondaryNav.removeClass('is-fixed');
+			card.removeClass('has-top-margin');
+		}
+	});
+
+});
